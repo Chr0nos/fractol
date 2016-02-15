702f897cfd7b2933b33bf965e570e1776d41f32a
Chr0nos <snicolet@student.42.fr>
2016-02-15 17:19:07 +0100

    ajout de a base de mandelbrot

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,32 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   mandelbrot.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/13 01:08:11 by snicolet          #+#    #+#             */
-/*   Updated: 2016/02/15 16:58:43 by snicolet         ###   ########.fr       */
+/*   Created: 2016/02/15 16:28:12 by snicolet          #+#    #+#             */
+/*   Updated: 2016/02/15 17:18:13 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
-# include "draw.h"
+#include "fractol.h"
 
-typedef struct	s_complex
+void				mandelbrot(t_context *c)
 {
-	float		real;
-	float		ima;
-}				t_complex;
+	int		n;
 
-typedef struct	s_contect
-{
-	t_mlx		*x;
-}				t_context;
-
-void			mandelbrot(t_context *c);
-int				key_down(int keycode, void *userdata);
-int				main(int ac, char **av);
-
-#endif
+	n = 50;
+	
+}
