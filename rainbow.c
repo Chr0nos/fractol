e049a42653a21fa7c3f6a89c2379a58049df1a81
Sebastien N <snicolet@student.42.fr>
2016-03-17 12:18:12 +0100

    mise a la norme

--- rainbow.c
+++ rainbow.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/17 12:12:31 by snicolet          #+#    #+#             */
-/*   Updated: 2016/03/17 12:00:10 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/03/17 12:17:29 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 
 void	rainbow(t_context *c)
 {
-	t_point		px;
-	int			h;
-	int			color;
-	t_rgb		rgb;
+	t_point			px;
+	int				h;
+	int				color;
+	t_rgb			rgb;
+	t_fracval		zoy;
 
+	zoy = (c->zoom_offsets.y < 0) ? -c->zoom_offsets.y : c->zoom_offsets.y;
 	px.y = 0;
 	h = 0;
-	if (c->zoom_offsets.y < 0.0f)
-		c->zoom_offsets.y = (t_fracval)0.0f;
 	while (px.y < c->x->height)
 	{
 		px.x = 0;
 		rgb = draw_color_hsv((int)((float)(h++ / 2.2f) *
-			c->zoom + (c->zoom_offsets.y * 60.0f)), 1.0f, 1.0f);
+			c->zoom + (zoy * 60.0f)), 1.0f, 1.0f);
 		color = draw_color_rgb2int(&rgb);
 		while (px.x < c->x->width)
 		{
