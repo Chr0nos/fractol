b18b478ce6e369e4679b9ae83885eab1db8e9a9a
Sébastien Nicolet <snicolet@student.42.fr>
2016-03-21 11:30:55 +0100

    fix du bug de zoom quand la profondeur etait trop basse pour un float

--- defaults.c
+++ defaults.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/19 01:40:37 by snicolet          #+#    #+#             */
-/*   Updated: 2016/03/16 15:35:24 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/03/21 11:28:51 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 void	set_defaults(t_context *c)
 {
 	c->zoom = 1.0f;
-	c->zoom_offsets = draw_make_vector(0.0f, 0.0f, 0.0f);
+	c->zoom_offsets.x = 0.0f;
+	c->zoom_offsets.y = 0.0f;
+	c->zoom_offsets.z = 0.0f;
 	c->iterator_offset = 1;
 	c->color_offset = 0;
-	c->mouse = draw_make_vector(0.0f, 0.0f, 0.0f);
-	c->center = draw_make_vector((float)(c->x->width / 2),
-		(float)(c->x->height / 2), 0.0f);
+	c->mouse.x = 0.0f;
+	c->mouse.y = 0.0f;
+	c->mouse.z = 0.0f;
+	c->center.x = (float)c->x->width / 2;
+	c->center.y = (float)c->x->height / 2;
+	c->center.z = 0.0f;
 }
--- headers/context.h
+++ headers/context.h
@@ -6,13 +6,20 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/17 17:40:29 by snicolet          #+#    #+#             */
-/*   Updated: 2016/03/17 18:12:59 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/03/21 11:26:28 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTEXT_H
 # define CONTEXT_H
 # include "fractol.h"
+
+typedef struct		s_dvector
+{
+		t_fracval	x;
+		t_fracval	y;
+		t_fracval	z;
+}					t_dvector;
 
 typedef struct		s_context
 {
@@ -21,12 +28,13 @@ typedef struct		s_context
 	void			(*post_display)(struct s_context *);
 	int				*colormap;
 	t_fracval		zoom;
-	t_vector		zoom_offsets;
+	t_dvector		zoom_offsets;
 	int				color_offset;
 	unsigned int	iterator_offset;
 	int				keys_press;
-	t_vector		mouse;
-	t_vector		center;
+	int				padding;
+	t_dvector		mouse;
+	t_dvector		center;
 	size_t			flags;
 }					t_context;
 
